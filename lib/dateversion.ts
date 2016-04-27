f47fd7b9d8699944249cccac19e11447d9a4da76
Rafael Salguero <rafaelsalgueroiturrios@gmail.com>
2016-04-26 17:54:00 -0700

    removed console output

--- lib/dateversion.ts
+++ lib/dateversion.ts
@@ -43,7 +43,6 @@ else {
         "\n//variable   : " + variable;
 
     var vars = variable.split(".");
-    console.log(vars.length);
     var fullName = "";
     for (var i = 0; i < vars.length; i++) {
         var n = vars[i];
@@ -60,7 +59,6 @@ else {
             }
         }
         else if (i == (vars.length - 1)) {
-            console.log(version);
             data += fullName + " = '" + version + "'";
     }
         else {
